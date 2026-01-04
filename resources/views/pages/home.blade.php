6b8d4222bb61da5ade64b9f9fe1cadaf7c286f73
Njie <njie@wak-studio.com>
2026-01-03 22:21:35 -0400

    txt

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,7 +12,7 @@
             </div>
             <div class="col-lg-4 text-center">
                 <div class="paper-patch" style="transform: rotate(3deg);">
-                    <p class="typewriter">"Ici, on ne fait pas juste des objets. On crée des souvenirs visuels qui ne s'effacent jamais."</p>
+                    <p class="typewriter">"La JMJ, ce n’est pas seulement de la personnalisation. C’est l’art de donner une âme aux objets."</p>
                     <span class="handwritten">— La JMJ Team</span>
                 </div>
             </div>
@@ -27,8 +27,11 @@
             <div class="col-md-8">
                 <div class="paper-patch">
                     <h2 class="mb-4">Le Concept <span class="handwritten">(en bref)</span></h2>
-                    <p class="lead">La JMJ est un studio créatif spécialisé dans la <strong>sublimation haute définition</strong>. On travaille avec le Bleu Nuit pour la profondeur, l'Or pour l'éclat, et le Blanc pour la pureté.</p>
-                    <p class="typewriter mt-4">Pourquoi nous ? Parce qu'on déteste le banal. On veut du brut, du vrai, du stylé. Que ce soit un mug pour ton café du matin ou une série limitée pour ton shop, on y met la même rage créative.</p>
+                    <p class="lead">La JMJ est un studio créatif spécialisé dans la <strong>sublimation haute définition</strong> dédié à la création d’objets personnalisés à forte valeur émotionnelle.</p>
+                    <p class="lead">Notre identité visuelle repose sur trois couleurs fortes :<strong>le Bleu Nuit, symbole de profondeur et d’élégance,
+l’Or, pour l’éclat et l’excellence,
+et le Blanc, pour la pureté et l’intemporalité.</strong> dédié à la création d’objets personnalisés à forte valeur émotionnelle.</p>                    
+                    <p class="typewriter mt-4">Pourquoi nous ? Parce que nous privilégions l’authenticité, la créativité brute et le style assumé. Qu’il s’agisse d’un mug pour accompagner un café du matin ou d’une série limitée destinée à une boutique, chaque projet est traité avec la même exigence, la même passion et une véritable énergie créative.</p>
                     <a href="/objets" class="btn-ink mt-4 d-inline-block">Découvrir le catalogue</a>
                 </div>
             </div>
